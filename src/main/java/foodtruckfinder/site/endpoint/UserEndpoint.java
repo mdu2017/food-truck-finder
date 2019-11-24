e61ee8f8cf1d5608dfcba1e0863a59ee43e5d405
Weston Straw <weston.straw@gmail.com>
2019-11-24 15:21:19 -0600

    merging

--- src/main/java/foodtruckfinder/site/endpoint/UserEndpoint.java
+++ src/main/java/foodtruckfinder/site/endpoint/UserEndpoint.java
@@ -136,7 +136,6 @@ public class UserEndpoint {
 	@PostMapping(value = "/notificationStatus", produces = "application/json")
 	public Boolean changeNotificationStatus(Long user_ID, Long truck_ID,
 											Integer[] sent){
-		System.out.println(user_ID + " " + truck_ID);
 		LocalDateTime time = LocalDateTime.of(sent[0], sent[1], sent[2],
 				sent[3], sent[4], sent[5]);
 		return userService.changeNotificationStatus(user_ID, truck_ID, time);

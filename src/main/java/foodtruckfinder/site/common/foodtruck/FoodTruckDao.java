5d3bca7d4a99f2ed403ee0d01d1ed2689e83b2c5
Andrew Case <andrewc0128@gmail.com>
2019-12-05 16:13:24 -0600

    Fixed Add FT without Menu

--- src/main/java/foodtruckfinder/site/common/foodtruck/FoodTruckDao.java
+++ src/main/java/foodtruckfinder/site/common/foodtruck/FoodTruckDao.java
@@ -212,8 +212,10 @@ public class FoodTruckDao {
 			updateSchedule(foodTruck);
 
 			//deal stuff
-			for(Deal d : foodTruck.getDeals()){
-				insertDeal(d);
+			if(foodTruck.getDeals() != null) {
+				for(Deal d : foodTruck.getDeals()){
+					insertDeal(d);
+				}
 			}
 
 			//original starting example code
@@ -699,7 +701,7 @@ public class FoodTruckDao {
 	private void updateMenu(FoodTruckDto foodTruck){
 		//Need to remove any stops not still present -- do this by removing all tuples associated with the
 		// food truck then adding them all back :)
-		String deleteSql = "DELETE * FROM MENU WHERE TRUCK_ID = :truckid";
+		String deleteSql = "DELETE FROM MENU WHERE TRUCK_ID = :truckid";
 		Map<String, ?> deleteparams = _Maps.map("truckid", foodTruck.getId());
 		jdbcTemplate.update(deleteSql, deleteparams);
 
@@ -766,7 +768,7 @@ public class FoodTruckDao {
 
 				//update the schedule table after the stops table has been updated
 				Map<String, ?> schedparams = _Maps.map(
-						"foodTruckId", foodTruck.getId(),
+						"foodTruckid", foodTruck.getId(),
 						"day", s.getFirst(),
 						"stopid", s.getSecond().getId());
 
